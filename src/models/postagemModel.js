2c668bb2a745470f2af49667d46bc4be0be2742d
gegissx <geovanna.paschoal@sptech.school>
2026-05-26 00:43:44 -0300

    configuração das KPIS na dashboard, e mudança nas cores dos sentimentos

--- src/models/postagemModel.js
+++ src/models/postagemModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function listar() {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucaoSql = `
-        SELECT * FROM viewPostagens;
+        SELECT * FROM vw_postagens;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -12,7 +12,7 @@ function listar() {
 function listarPorUsuario(idUsuario) {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucaoSql = `
-        SELECT * FROM viewPostagens WHERE idUsuario = ${idUsuario};
+        SELECT * FROM vw_postagens WHERE idUsuario = ${idUsuario};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -21,7 +21,7 @@ function listarPorUsuario(idUsuario) {
 function listarMemorias(idUsuario) {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucaoSql = `
-        SELECT * FROM viewPostagens WHERE idUsuario = ${idUsuario} AND importante = 1;
+        SELECT * FROM vw_postagens WHERE idUsuario = ${idUsuario} AND importante = 1;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
